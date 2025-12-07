493624d3cac699f8a95e72aa9353f0d2e7ab5e13
lois27-es <geraldinelois.agulto@email.lcup.edu.ph>
2025-12-07 19:05:21 +0800

    Fix case-sensitive Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
  import React from 'react'
 import Navbar from './components/navbar/Navbar'
-import Hero from "./components/hero/Hero";
+import Hero from "./components/hero/hero";
 import About from "./components/about/About";
 import Services from "./components/Services/Services";
 import MyProject from './components/MyProject/MyProject';
