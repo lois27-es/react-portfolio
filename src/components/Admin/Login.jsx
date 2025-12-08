eb94fed4396a6cca9bdd739f88ba683d486f06d4
kyrajeanbassig <kyrajean.bassig@email.lcup.edu.ph>
2025-12-09 01:43:54 +0800

    updated

--- src/components/Admin/Login.jsx
+++ src/components/Admin/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from '../supabaseClient'; 
-import './login.css'; 
+import './Login.css'; 
 
 const Login = () => {
   const [email, setEmail] = useState('');
